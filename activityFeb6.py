f5b3442e49b0d2c83931ec752ea46be51377110a
Dsmith-lockett11th <158069704+Dsmith-lockett11th@users.noreply.github.com>
2024-02-08 16:19:09 +0000

    Fed8classwork

---
+++ activityFeb6.py
@@ -0,0 +1,18 @@
+# what is complexity ?
+# the measure of how resource
+# intensive a program can
+
+# what is abstraction?
+# abstraction is the process of
+# simplifying or hiding complexity
+
+name = input("what is your name")
+teeshirtsize=imput('what is your tee shirt')
+
+# Camel Case = every new word
+# after the first word is capitalized.
+teeShirtsizeSM= False
+teeShirtsizeMd= False
+teeShirtsizeLg= True
+
+
