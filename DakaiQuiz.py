63eb028608cbf6b6269f67cc8f298247df3b3de6
Dsmith-lockett11th <158069704+Dsmith-lockett11th@users.noreply.github.com>
2024-02-16 15:58:36 +0000

    Quiz1Feb16

---
+++ DakaiQuiz.py
@@ -0,0 +1,63 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+print()
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+input()
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+
+#4. What does it mean when you encounter a syntax error?
+'it is something the computer dont understand.'
+#5. What is a variable? Provide an example of a variable in code.
+'variables are placeholders/ containers for data (datatypes)'
+#6. Write a function in Python that when called will print out 
+# your name.
+print('Dakai Smith-lockett')
+#7. What is a function definition and a function call?
+"A function is a block of code which only runs when it is called."
+#8. Describe what each symbol means?
+
+# = // single equal 
+"it means that it is an assignment"
+# == // double equal
+"it means that it is equal to a operator"
+# != // exclaimation equal
+'not equal'
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+"you will get a typeError "
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+'the output will be true'
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+Bookbagitemslist = ["Books", "pencils", "erasers", "calculator", "paper"]
+Bookbagitemsprice = [5, 6, 3, 15, 10,]
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+' Pascal case, camel case, snake case'
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+'you will use when making a function'
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+"when the code block is using and & or"
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+
+# Bonus : What does the type() function do?
+"returns the object that you input"
--- kickoff.py
+++ kickoff.py
@@ -1,2 +1,4 @@
-"Dakai Smith"
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
 
+print(listOne == listTwo)
