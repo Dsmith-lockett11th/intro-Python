340f4be33af8c4180016b9db7b3e8761845fc8bf
Dsmith-lockett11th <158069704+Dsmith-lockett11th@users.noreply.github.com>
2024-03-07 16:20:00 +0000

    mar7notes

---
+++ classreviewnotes.py
@@ -0,0 +1,26 @@
+# programs = instrucions for computers.
+
+#programming languages = differnet ways we can write and provide instructions
+
+# syntax = the formatting riles of a language. in this case, a programming language
+
+# additional note - when we are writing programs all we're doing is talking to the computer and giving it instructions to complete a task.
+
+# how to write a function in ptthon
+
+def myfunction():
+    print("hello")
+
+
+# how to write a function in javascript
+    
+#function myfunction(){
+    console.log()
+#}
+
+
+# a variable = containers for data
+# data are just data types
+
+# data types = the basic building blocks for our programming language basically 
+# programming language alphabet
